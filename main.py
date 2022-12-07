b7803bdd65f58c7ec849354487e29cd30a01329f
Alejandrito97 <alex.hreus@gmail.com>
2022-12-07 09:43:00 -0800

    script prints report about the book/text

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 book_path = "books/frankenstein.txt"
 
 def main():
-
+    
     def open_and_read(path):
         with open(path) as file:
             book_text = file.read()
             return book_text
 
+    def count_words(text_book):
+        words = text_book.split()
+        return len(words)
 
-    def character_count(text):
-        
+    def character_count(text):        
         characters_dictionary = {}
         letters = list(text)
 
@@ -21,9 +23,29 @@ def main():
 
         return characters_dictionary 
            
+    def dict_to_list(dict_to_convert):
+        
+        new_list = []
+        for key, val in dict_to_convert.items():
+            if key.isalpha():
+                new_list.append([key, val])
+
+        new_list.sort(key= lambda a: a[1], reverse= True)
+        return new_list
+    
     text_to_count = open_and_read(book_path).lower()
+
+    number_of_words = count_words(text_to_count)
     
     characters = character_count(text_to_count)
-    print(characters) 
+
+    sorted_list = dict_to_list(characters)
+    
+    print("--- Report of books/frankenstein.txt ---")
+    print(f"{number_of_words} words found in the document")
+    print(" ")
+    for i in range(len(sorted_list)):
+        print(f"The {sorted_list[i][0]} character was found {sorted_list[i][1]} times")
+    print("--- End report ---")
 
 main()
